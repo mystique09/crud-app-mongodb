9cae55fe7e46aa4e7a5154129214ffd5e587e644
mystique09 <knightkrypto7@gmail.com>
2021-10-17 00:02:09 +0800

    added mockdatabase

---
+++ mockDB.js
@@ -0,0 +1,9 @@
+
+const mockUsers = [
+  {id: 1, username: "test1", password: "test1", email: "test1@gmail.com"},
+  {id: 2, username: "test2", password: "test2", email: "test2@gmail.com"},
+  {id: 3, username: "test3", password: "test3", email: "test3@gmail.com"},
+  {id: 4, username: "test4", password: "test4", email: "test4@gmail.com"},
+];
+
+module.exports = mockUsers;
