2521277206b21656da7f16351f8ecff21b66f003
mystique09 <knightkrypto7@gmail.com>
2021-10-17 00:02:28 +0800

    added routes

--- routes/users.js
+++ routes/users.js
@@ -1,9 +1,16 @@
 const express = require('express');
 const router = express.Router();
+const UserController = require('../controllers/user.controller');
 
 /* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
-});
+router.get('/', UserController.getUsers);
+
+router.get('/:id', UserController.getUser);
+
+router.post('/add', UserController.addUser);
+
+router.patch('/update', UserController.updateUser);
+
+router.delete('/delete', UserController.deleteUser);
 
 module.exports = router;
